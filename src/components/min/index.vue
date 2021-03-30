853ca08bfd1023c353dce2a63298ee1c9666999f
YuRongzhao <yurz@zhongketaiyue.com>
2021-03-30 15:10:05 +0800

    新我的

---
+++ src/components/min/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="min">
+    <min-top></min-top>
+  </div>
+</template>
+
+<script>
+import MinTop from './components/min-top/min-top'
+export default {
+  data() {
+    return {
+    };
+  },
+  components: {
+    MinTop
+  },
+  methods: {
+  },
+};
+</script>
+<style lang="stylus" scoped type="text/stylus">
+@import '~@/common/stylus/variable.styl';
+
+</style>
