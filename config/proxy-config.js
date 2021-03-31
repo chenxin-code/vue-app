2ef685ff1a9bdd54b927b2c50d3362dc6f690da0
hjc <huangjc@zhongketaiyue.com>
2021-04-01 03:11:45 +0800

    change text height

--- config/proxy-config.js
+++ config/proxy-config.js
@@ -17,8 +17,8 @@ module.exports = {
         '/api': {
             // target: 'https://mall-prod-app-linli.timesgroup.cn',
             // target: 'http://192.168.31.118:18807',
-            target: 'http://39.105.84.126:28807',
-            // target: 'http://mall-uat-app-linli.timesgroup.cn/',
+            // target: 'http://39.105.84.126:28807',
+            target: 'http://mall-uat-app-linli.timesgroup.cn/',
             // target: 'http://tbdapp.deepermobile.com', // 原来的测试地址
             // target:'http://henansydemo.deepermobile.com',
             // target: 'http://jlapp.95504.net:81',
@@ -29,10 +29,10 @@ module.exports = {
         },
         '/app': {
             // target: 'http://tbdapp.deepermobile.com',
-            target: 'http://39.105.84.126:28807',
+            // target: 'http://39.105.84.126:28807',
             // target: 'http://192.168.31.118:18807',
             // target: 'https://mall-prod-app-linli.timesgroup.cn',
-            // target: 'http://mall-uat-app-linli.timesgroup.cn/',
+            target: 'http://mall-uat-app-linli.timesgroup.cn/',
             changeOrigin: true,
         },
     }
