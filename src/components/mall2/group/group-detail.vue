5722e779590c06a4ae5e85589aa726e69ec0fe4c
hjc <huangjc@zhongketaiyue.com>
2021-04-26 15:13:12 +0800

    小程序分享页面地址更改

--- src/components/mall2/group/group-detail.vue
+++ src/components/mall2/group/group-detail.vue
@@ -371,7 +371,7 @@ export default {
       appShare
         .shareForOpenWXMiniProgram({
           userName: "gh_28d617271c97",
-          path: `pages/weView/weView?redirect=${encodeURIComponent(
+          path: `pages/common/home/index?redirect=${encodeURIComponent(
             `/app-vue/app/index.html#/groupproduct?skuId=${this.$route.query.skuId}&productType=${this.$route.query.productType}&groupId=${this.$route.query.groupId}&mktGroupBuyId=${this.groupDetail.mktGroupBuyId}&spuId=${this.groupDetail.spuId}`
           )}`,
           title: shareData.title,
