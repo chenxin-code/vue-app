3512b844c375a0eaaf24713fae03e20ae320b9f1
hjc <huangjc@zhongketaiyue.com>
2021-07-06 21:42:22 +0800

    change console

--- babel.config.js
+++ babel.config.js
@@ -12,9 +12,9 @@ const plugins = [
     "vant"
   ]
 ];
-if (IS_PROD) {
-  plugins.push("transform-remove-console");
-}
+// if (IS_PROD) {
+//   plugins.push("transform-remove-console");
+// }
 
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
