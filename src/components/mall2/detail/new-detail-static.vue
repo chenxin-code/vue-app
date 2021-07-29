2ed098319843ec71a385e53a92b5a9197c492443
hjc <huangjc@zhongketaiyue.com>
2021-07-29 21:32:13 +0800

    change share show

--- src/components/mall2/detail/new-detail-static.vue
+++ src/components/mall2/detail/new-detail-static.vue
@@ -1563,7 +1563,7 @@
             <img src="static/image/mall2/share_wechat.png" alt="" />
             <div>微信好友</div>
           </div>
-          <div class="share_botton_item" @click="shareImg" v-show="false">
+          <div class="share_botton_item" @click="shareImg" v-show="true">
             <img src="static/image/mall2/share_img.png" alt="" />
             <div>图片分享</div>
           </div>
