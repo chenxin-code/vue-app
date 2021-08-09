f6e3f4ca24cdda1545742a38f53411bab9ea1520
chenshuimei <1913405299@qq.com>
2021-08-09 11:14:53 +0800

    fix

--- src/components/mall2/list/list.vue
+++ src/components/mall2/list/list.vue
@@ -641,14 +641,12 @@ export default {
       );
     },
     backEvent: function() {
-      console.log(`this.$store.state.webtype`,this.$store.state.webtype);
-      console.log(`backEvent $route.query`,this.$route.query);
       if (this.$route.query.backApp) {
         appNav.navigatorBack({ url: "0" }).then(res => {
           console.log(res);
         });
       } else {
-        if(this.$route.query.searchFrom == "coupon" && this.$store.state.webtype === "1") {
+        if(this.$route.query.searchFrom == "coupon" && this.$store.state.webtype == "0") {
           console.log(`searchFrom`);
           //从AppcMember微应用跳转过来的，点击返回要回到AppcMember
           nativeRouter.openTargetRouter({
