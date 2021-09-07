ef2443d4fe36e73082ad59d3c021a424a5ec679c
chenxin <chenxin@gennlife.com>
2021-09-07 16:14:34 +0800

    订单页面优化

--- src/components/mall2/placeorder/placeorder.vue
+++ src/components/mall2/placeorder/placeorder.vue
@@ -284,7 +284,7 @@
           </div>
 
           <div class="concatStore" style="background-color: #f1f1f1;"
-               :style="showMini ? { 'margin-top' : '58px'} : { 'margin-top' : '112px'}"
+               :style="index === 0 ? (showMini ? { 'margin-top' : '58px'} : { 'margin-top' : '112px'}) : ''"
                v-for="(item,index) in occurList" :key="index">
             <!-- <div class="block-div">{{item.store[0].storeName}}</div> -->
             <div
