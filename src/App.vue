01fe3ba64f91db86b287e1ff06d72a471670c26a
zhaowenli <sushiduli92@hotmail.com>
2021-09-13 11:57:03 +0800

    分享码

--- src/App.vue
+++ src/App.vue
@@ -153,7 +153,7 @@ export default {
         });
     }
     console.log(initObj);
-    this.referrerCode=initObj.referrerCode;
+    this.referrerCode=initObj.referrerCode || initObj.shareCode;
     if (this.$store.state.webtype == "2" || this.$store.state.webtype == "3") {
       // let initObj = {};
       // console.log("localtion href", location.href);
@@ -299,7 +299,7 @@ export default {
     updateReferrerCode(){
       let distributionPersonDetail=this.$store.state.distributionPersonDetail;
       let params={
-        distributorShareCode:this.referrerCode || this.$route.query.referrerCode || -1,
+        distributorShareCode:this.referrerCode || -1,
         referrerId: distributionPersonDetail.distributorId,
         referrerFatherId: distributionPersonDetail.parentDistributorId
       };
